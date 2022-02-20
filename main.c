78596a2f06cb3452288aedc5b99d15ec2943bfaf
Tyler Storr <tystorr@gmail.com>
2022-02-20 04:50:51 -0500

    reformat and add comments

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <malloc.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <wait.h>
 
-void break_string(char *input, char *components[]);
+void break_string(char *input_string, char *components[]);
 
 void exit_shell();
 
@@ -17,62 +16,66 @@ void change_directory(char *components[]);
 
 void change_path(char *components[]);
 
-bool check_path(char *command_path, char *components[]);
+bool check_path(char *check_path, char *components[]);
 
-char *input_line;
-char *path[30];
-char error_message[30] = "An error has occurred\n";
+
+#define PATH_SIZE 30
+char *read_string;
+char *path[PATH_SIZE];
+
 
 int main(int argc, char *argv[]) {
     size_t buffer_size = 50;
     bool exit = false;
-    int line_num = 1;
-    char *separated_strings[99];
-    char command_path[50];
+    char *separated_components[buffer_size];
+    char command_path[buffer_size];
 
     // set up initial path
-    path[1] = "/bin";
+    path[0] = "/bin";
 
-    input_line = malloc(buffer_size * sizeof(char));
+    read_string = malloc(buffer_size * sizeof(char));
 
     // if there is nothing in the command line, attempt to read file and redirect to stdin
     if (argc != 1) {
         stdin = freopen(argv[1], "r", stdin);
+        // Could not open file
+        if (stdin == NULL) {
+            print_error();
+            exit_shell();
+        }
     }
-    if (stdin == NULL) {
-        print_error();
-        exit_shell();
-    }
+
     while (exit == false) {
+        // interactive mode
         if (argc == 1) {
             printf("wish> ");
         }
         // Get input from stdin
-        int result = (int) getline(&input_line, &buffer_size, stdin);
-        break_string(input_line, separated_strings);
+        int result = (int) getline(&read_string, &buffer_size, stdin);
+        break_string(read_string, separated_components);
 
-        if (strcmp(separated_strings[0], "exit") == 0 || result == -1) {
-            if (separated_strings[1] != NULL) {
+        if (strcmp(separated_components[0], "exit") == 0 || result == -1) {
+            if (separated_components[1] != NULL) {
                 print_error();
             } else {
                 exit = true;
             }
-        } else if (strcmp(separated_strings[0], "cd") == 0) {
-            change_directory(separated_strings);
-        } else if (strcmp(separated_strings[0], "path") == 0) {
-            change_path(separated_strings);
+        } else if (strcmp(separated_components[0], "cd") == 0) {
+            change_directory(separated_components);
+        } else if (strcmp(separated_components[0], "path") == 0) {
+            change_path(separated_components);
         } else {
-            int rc = fork();
-
-            if (rc < 0) {
+            // any other shell command
+            int return_pid = fork();
+            if (return_pid < 0) {
                 // fail to fork
                 exit_shell();
-            } else if (rc == 0) {
+            } else if (return_pid == 0) {
                 // in the universe of the child
-                if (check_path(command_path, separated_strings) == false) {
+                if (check_path(command_path, separated_components) == false) {
                     print_error();
                 }
-                execv(command_path, separated_strings);
+                execv(command_path, separated_components);
                 exit = true;
             } else {
                 // in the universe of the parent
@@ -82,18 +85,23 @@ int main(int argc, char *argv[]) {
     }
 
     exit_shell();
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-void break_string(char *input, char *components[]) {
+/**
+ * Break an input_string string into components removing the newline if it exists
+ * @param input_string input_string string to break
+ * @param components address of where to stash the components
+ */
+void break_string(char *input_string, char *components[]) {
     char delimiter = ' ';
     int i = 0;
     bool exit = false;
 
     // remove newline if it exists
-    input[strcspn(input, "\n")] = 0;
+    input_string[strcspn(input_string, "\n")] = 0;
     while (exit == false) {
-        components[i] = strsep(&input, &delimiter);
+        components[i] = strsep(&input_string, &delimiter);
         if (components[i] == NULL) {
             exit = true;
         } else {
@@ -102,41 +110,55 @@ void break_string(char *input, char *components[]) {
     }
 }
 
+/**
+ * Close and free file pointers and exit gracefully
+ */
 void exit_shell() {
-    if (stdin != NULL) {
-        fclose(stdin);
-    }
+    fclose(stdin);
     fclose(stdout);
     fclose(stderr);
-    free(input_line);
-    exit(0);
+    free(read_string);
+    exit(EXIT_SUCCESS);
 }
 
+/**
+ * Print an error message to stderr
+ */
 void print_error() {
+    char *error_message = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
+/**
+ * Built in command Change Directory
+ * @param components input arguments of which directory to go to
+ */
 void change_directory(char *components[]) {
+    // if no arguments to cd or cd with more than 1 argument
     if (components[1] == NULL || components[2] != NULL) {
         print_error();
     }
+    // if directory does not exist
     if (chdir(components[1]) == -1) {
         errno = ENOTDIR;
     }
 }
 
+/**
+ * Change the existing path variable
+ * @param components arguments for the new path to be changed to
+ */
 void change_path(char *components[]) {
     bool exit = false;
-    int i = 1;
+    int i = 0;
 
-    if(components[1] == NULL){
-        path[1] = "";
-        return;
-    }
     while (exit == false) {
         if (components[i] == NULL) {
+            // If nothing is entered into the path, set path to nothing
+            path[i] = NULL;
             exit = true;
         } else {
+            // Otherwise, set path variable to everything entered in path
             path[i] = malloc(strlen(components[i]));
             strcpy(path[i], components[i]);
             i++;
@@ -144,13 +166,19 @@ void change_path(char *components[]) {
     }
 }
 
-bool check_path(char *command_path, char *components[]) {
-    for (int i = 1; i < 30; ++i) {
+/**
+ * Check if an argument exists in the current path
+ * @param check_path full path to check for an argument
+ * @param components input arguments to check if command can be executed in the path
+ * @return true with the full path if exists within path. false if it does not exist in path
+ */
+bool check_path(char *check_path, char *components[]) {
+    for (int i = 0; i < PATH_SIZE; ++i) {
         if (path[i] != NULL) {
-            strcpy(command_path, path[i]);
-            strcat(command_path, "/");
-            strcat(command_path, components[0]);
-            if (access(command_path, X_OK) == 0) {
+            strcpy(check_path, path[i]);
+            strcat(check_path, "/");
+            strcat(check_path, components[0]);
+            if (access(check_path, X_OK) == 0) {
                 return true;
             }
         }
